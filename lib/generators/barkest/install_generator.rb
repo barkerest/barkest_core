4d96a3a2e88007e033fd5310de69ed018a06b3e8
Beau Barker <rtecoder@gmail.com>
2016-07-13 21:16:34 -0400

    Register installers in BarkestCore module by name.

--- lib/generators/barkest/install_generator.rb
+++ lib/generators/barkest/install_generator.rb
@@ -8,22 +8,14 @@ module Barkest
 
     desc 'Installs the Barkest functionality into your application.'
 
-    def self.register(installer)
-      installers << installer unless installers.include?(installer)
-    end
-
     def install_modules
-      installers.each do |inst|
+      BarkestCore.installers.each do |inst_name|
+        require 'generators/' + inst_name.underscore
+        inst = Object.const_get(inst_name).new
         sep = '-' + ('=---' * 19) + '=-'
         tell "#{sep}\nProcessing #{inst.class}\n#{sep}", :bold
         inst.public_methods(false).each { |method| inst.send(method) }
       end
-    end
-
-    private
-
-    def installers
-      @installers ||= [ BarkestCore::InstallGenerator ]
     end
 
   end
