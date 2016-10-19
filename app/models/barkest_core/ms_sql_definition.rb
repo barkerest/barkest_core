397d93d2405dd5740a5643d21a58f64130129494
Beau Barker <beau@ckindustrial.com>
2016-10-19 13:17:40 -0400

    Add version and type to MsSqlDefinition

--- app/models/barkest_core/ms_sql_definition.rb
+++ app/models/barkest_core/ms_sql_definition.rb
@@ -1,4 +1,12 @@
+require 'zlib'
+
 module BarkestCore
+
+  ##
+  # Contains a SQL definition to create a single table, view, function, or procedure.
+  #
+  # SQL source is not validated, however simple checks are made to ensure that only
+  # one DDL statement is present.
   class MsSqlDefinition
     InvalidDefinition       = Class.new(StandardError)
 
@@ -8,10 +16,25 @@ module BarkestCore
     UnmatchedBracket        = Class.new(InvalidDefinition)
     UnclosedQuote           = Class.new(InvalidDefinition)
     UnmatchedComment        = Class.new(InvalidDefinition)
+    MissingReturnType       = Class.new(InvalidDefinition)
 
-    attr_reader :name, :type, :definition
+    attr_reader :name, :type, :definition, :version, :return_type
 
-    def initialize(raw_sql)
+    def initialize(raw_sql, timestamp = 0)
+
+      @return_type = :table  # the default.  functions can be different.  procedures can be iffy since they may or may not return a result set.
+
+      if timestamp.is_a?(String)
+        timestamp = Time.new(timestamp)
+      end
+
+      if timestamp.is_a?(Date)
+        timestamp = "#{timestamp.year.to_s.rjust(4,'0')}#{timestamp.month.to_s.rjust(2,'0')}#{timestamp.day.to_s.rjust(2,'0')}0000".to_i
+      elsif timestamp.is_a?(Time)
+        timestamp = "#{timestamp.year.to_s.rjust(4,'0')}#{timestamp.month.to_s.rjust(2,'0')}#{timestamp.day.to_s.rjust(2,'0')}#{timestamp.hour.to_s.rjust(2,'0')}#{timestamp.min.to_s.rjust(2,'0')}".to_i
+      end
+
+      timestamp = 0 unless timestamp.is_a?(Fixnum)
 
       raw_sql = raw_sql.to_s.lstrip
       # strip leading comment lines.
@@ -35,14 +58,14 @@ module BarkestCore
       raise EmptyDefinition, 'raw_sql contains no data' if raw_sql.blank?
 
       # first line should be CREATE VIEW|FUNCTION|PROCEDURE "XYZ"
-      regex = /^CREATE\s+(?<TYPE>VIEW|FUNCTION|PROCEDURE)\s+\[?(?<NAME>[A-Z][A-Z0-9_]*)\]?\s+(?<DEFINITION>.*)$/im
+      regex = /^CREATE\s+(?<TYPE>VIEW|FUNCTION|PROCEDURE)\s+["\[]?(?<NAME>[A-Z][A-Z0-9_]*)["\]]?\s+(?<DEFINITION>.*)$/im
       match = regex.match(raw_sql)
 
       raise MissingCreateStatement, 'raw_sql must contain a "CREATE VIEW|FUNCTION|PROCEDURE" statement' unless match
 
       @name = match['NAME']
-      @type = match['TYPE']
-      @definition = match['DEFINITION']
+      @type = match['TYPE'].upcase
+      @definition = match['DEFINITION'].strip
 
       # we're going to test the definition loosely.
       # so first we'll get rid of all valid literals and comments.
@@ -69,14 +92,58 @@ module BarkestCore
       raise UnclosedQuote,      'raw_sql contains an unclosed string literal'                     if test_sql.include?("'")
       raise UnclosedQuote,      'raw_sql contains an unclosed ANSI quoted literal'                if test_sql.include?('"')
       raise UnmatchedComment,   'raw_sql contains a "/*" sequence with no matching "*/" sequence' if test_sql.include?('/*')
-      raise UnmatchedComment,   'raw_sql contains a "*/" sequence with no matching "*/" sequence' if test_sql.include?('*/')
+      raise UnmatchedComment,   'raw_sql contains a "*/" sequence with no matching "/*" sequence' if test_sql.include?('*/')
 
-      # and finally we can test for extra DDL.
-      # only the initial CREATE statement is allowed.
-      regex = /\s(create|alter|drop|grant)\s/im
-      if (match = regex.match(test_sql))
-        raise ExtraDDL, "raw_sql contains a #{match[1]} statement after the initial CREATE statement"
+      unless type == 'PROCEDURE'
+        # and finally we can test for extra DDL.
+        # only the initial CREATE statement is allowed.
+        regex = /\s(create|alter|drop|grant)\s/im
+        if (match = regex.match(test_sql))
+          raise ExtraDDL, "raw_sql contains a #{match[1]} statement after the initial CREATE statement"
+        end
       end
+
+      # and for functions, get the return type.
+      if type == 'FUNCTION'
+        regex = /\sRETURNS\s+(?:@[A-Z][A-Z0-9_]*\s+)?(?<TYPE>[A-Z][A-Z0-9_()]*)\s/im
+
+        match = regex.match(@definition)
+        raise MissingReturnType, 'raw_sql seems to be missing the RETURNS statement for the function.' unless match
+
+        rtype = match['TYPE'].downcase
+        rsize = 0
+        if rtype.include('(')
+          rtype,_,rsize = rtype.partition('(')
+          rsize = rsize.partition(')')[0].to_i
+        end
+
+        @return_type =
+            case rtype
+              when 'bit'
+                :boolean
+
+              when 'int', 'integer', 'bigint', 'smallint', 'tinyint'
+                :integer
+
+              when 'decimal', 'numeric', 'money', 'smallmoney'
+                :decimal
+
+              when 'float', 'real'
+                :float
+
+              when 'date', 'datetime', 'datetime2', 'time', 'smalldatetime', 'datetimeoffset'
+                :time
+
+              when 'char', 'varchar', 'text', 'nchar', 'nvarchar', 'ntext', 'binary', 'varbinary', 'image'
+                :string
+
+              else
+                rtype.to_sym
+            end
+      end
+
+      # set the version.
+      @version = timestamp.to_s + Zlib.crc32(@definition).to_s(16).ljust(8,'0')
 
     end
 
