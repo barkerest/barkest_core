0d575bc5d3fff8655b3fa29fe19af1fbe67b3a64
Beau Barker <beau@ckindustrial.com>
2016-11-25 11:40:15 -0500

    Add comments to mailers.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,3 +1,5 @@
+##
+# Provides a default application mailer that is easy to override if necessary.
 class ApplicationMailer < ::BarkestCore::ApplicationMailerBase
 
 end
--- app/mailers/barkest_core/contact_form.rb
+++ app/mailers/barkest_core/contact_form.rb
@@ -1,19 +1,20 @@
 require_dependency 'barkest_core/application_mailer_base.rb'
 
-class BarkestCore::ContactForm < ::BarkestCore::ApplicationMailerBase
+module BarkestCore
+  ##
+  # This mailer is used for the generic contact form.
+  class ContactForm < ::BarkestCore::ApplicationMailerBase
 
-  # Subject can be set in your I18n file at config/locales/en.yml
-  # with the following lookup:
-  #
-  #   en.barkest_core.contact_form.contact.subject
-  #
-  def contact(msg)
-    @data = {
-        msg: msg,
-        client_ip: msg.remote_ip,
-        gems: BarkestCore.gem_list(Rails.application.class.parent_name.underscore, 'rails', 'barkest*'),
-    }
-    mail subject: msg.full_subject, reply_to: msg.your_email
+    ##
+    # Sends the message from the contact form.
+    def contact(msg)
+      @data = {
+          msg: msg,
+          client_ip: msg.remote_ip,
+          gems: BarkestCore.gem_list(Rails.application.class.parent_name.underscore, 'rails', 'barkest*'),
+      }
+      mail subject: msg.full_subject, reply_to: msg.your_email
+    end
   end
 
 end
